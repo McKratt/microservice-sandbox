cdab5be0f57e07039c4c148f74842f1c4068ad5c
Stéphane Krattinger <stef.krattinger@gmail.com>
2019-01-29 22:19:26 +0100

    fix test

--- case/case-infra/src/test/java/net/bakaar/sandbox/cas/infra/service/CaseApplicationServiceTest.java
+++ case/case-infra/src/test/java/net/bakaar/sandbox/cas/infra/service/CaseApplicationServiceTest.java
@@ -7,6 +7,7 @@ import net.bakaar.sandbox.shared.domain.vo.PNumber;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -19,10 +20,12 @@ public class CaseApplicationServiceTest {
         CaseApplicationService service = new CaseApplicationService(domainService);
         PNumber pNumber = PNumber.of(98765432);
         CreateCaseCommand command = new CreateCaseCommand(pNumber);
+        Case expectedCase = mock(Case.class);
+        given(domainService.createCase(command)).willReturn(expectedCase);
         //When
         Case returnedCase = service.createCase(command);
         //Then
-        assertThat(returnedCase.getInjured()).isSameAs(pNumber);
+        assertThat(returnedCase).isSameAs(expectedCase);
         verify(domainService).createCase(command);
     }
 }
