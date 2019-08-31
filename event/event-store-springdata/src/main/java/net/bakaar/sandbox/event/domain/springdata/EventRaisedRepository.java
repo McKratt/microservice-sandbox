3e0d93733b6fee80123fcf867e2bf7be08c860b8
Stéphane Krattinger <stef.krattinger@gmail.com>
2019-08-31 06:32:11 +0200

    remove old event project

--- event/event-store-springdata/src/main/java/net/bakaar/sandbox/event/domain/springdata/EventRaisedRepository.java
+++
@@ -1,6 +0,0 @@
-package net.bakaar.sandbox.event.domain.springdata;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EventRaisedRepository extends CrudRepository<DBEventRaised, Long> {
-}
