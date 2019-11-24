486066d60cbeff82d7ff86fe56193145a78dfeb9
Stéphane Krattinger <stef.krattinger@gmail.com>
2019-11-24 19:30:42 +0100

    change github action, cleanup

--- business-number/src/test/java/net/bakaar/sandbox/businessnumber/BusinessNumberApplicationIT.java
+++ business-number/src/test/java/net/bakaar/sandbox/businessnumber/BusinessNumberApplicationIT.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class BusinessNumberApplicationIT {
 
     @Test
-    void contextLoads() throws Exception {
+    void contextLoads() {
         // Check only that the context correctly load...
     }
 
